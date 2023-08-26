c4dd48ee04c596068de2de1fe0891b426872f40e
Milos Roksandic <milos.roksandic@computerrock.com>
2023-08-26 14:44:46 +0200

    creating user for gateway service and adding dependencies

--- docker/postgres/init-user-db.sh
+++ docker/postgres/init-user-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER docker;
-	CREATE DATABASE docker;
-	GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+	CREATE USER user_service_user WITH PASSWORD 'user_pwd';
+	CREATE DATABASE user_service_db;
+	GRANT ALL PRIVILEGES ON DATABASE user_service_db TO user_service_user;
 EOSQL
