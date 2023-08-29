2fb1ecddac7a267a5372ec10ab26fd0faaf21541
Milos Roksandic <milos.roksandic@computerrock.com>
2023-08-29 16:21:10 +0200

    Added api gateway service

---
+++ gateway/src/Controller/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api', name: 'api')]
+    public function index(Request $request): Response
+    {
+        return new Response('OK');
+    }
+}
